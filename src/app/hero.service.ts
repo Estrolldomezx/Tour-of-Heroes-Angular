585bf215118318a8897a29a1f5650ece799b4bb4
Wanish Sengkhim <wanish45037.2@gmail.com>
2021-04-21 04:06:45 +0700

    Add Services

---
+++ src/app/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root' //provide service at root level, inject instance of HeroService into any class that asks for it
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+}
